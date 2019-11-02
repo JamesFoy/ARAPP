41f3003cd632004fe03805a7f922d96455b0abbd
James Home <jamesfoy17>
2019-11-02 13:23:12 +0000

    Added A Virtual Button
    Added a virtual button to the ar app which when aimed at the witcher 3 poster will appear. When pressing the button with your finger in the real world the trailer for the witcher 3 will play.

---
+++ Assets/Scripts/VirutalButtonInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirutalButtonInteraction : MonoBehaviour, IVirtualButtonEventHandler
+{
+    GameObject vbObj;
+    [SerializeField]
+    GameObject videoPlayerPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbObj = GameObject.Find("VirtualButton");
+        vbObj.GetComponentInChildren<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        videoPlayerPlane.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        videoPlayerPlane.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {       
+        throw new System.NotImplementedException();
+    }
+}
