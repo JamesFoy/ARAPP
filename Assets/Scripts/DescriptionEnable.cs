3073c39a6468614a1fcc9bafc069a5fee63bde14
JamesFoy <39011272+JamesFoy@users.noreply.github.com>
2019-11-15 15:24:53 +0000

    Added UI Button and Text

---
+++ Assets/Scripts/DescriptionEnable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class DescriptionEnable : MonoBehaviour
+{
+    public GameObject descriptionTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        descriptionTxt.SetActive(false);
+    }
+
+    public void OnButtonPress()
+    {
+        if (descriptionTxt.activeInHierarchy)
+        {
+            descriptionTxt.SetActive(false);
+        }
+        else
+        {
+            descriptionTxt.SetActive(true);
+        }
+    }
+}
